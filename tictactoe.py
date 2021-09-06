2098afadb476168e6618db1f0cebb4de38eb57a7
anvilly <anvillyh@gmail.com>
2021-09-06 16:06:06 +1200

    game runs smoothly,needs to optimise comp gameplay

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 import pygame
 from globalvar import *
 dis, turn
@@ -11,6 +12,7 @@ dis, turn
 X = "X"
 O = "O"
 EMPTY = None
+best_move=()
 
 # Colors
 white=(255,255,255)
@@ -30,25 +32,6 @@ def empty(board):
 
     return elements
 
-def draw_x(board,i,j):
-    centerX = j * 80 + 180+40
-    centerY = i * 80 + 80+40
-    pygame.draw.line (dis, white, (centerX - 22, centerY - 22),
-        (centerX + 22, centerY + 22), 2)
-
-    pygame.draw.line (dis, white, (centerX + 22, centerY - 22),
-        (centerX - 22, centerY + 22), 2)
-    board[i][j]=turn
-    print("player is this")
-    print(board[i][j])
-
-
-def draw_o(board,i,j):
-    centerX = j * 100 + 50
-    centerY = i * 100 + 50
-    pygame.draw.circle (dis, white, (centerX, centerY), 44, 2)
-    board[i][j]=turn
-
 
 def initial_state():
     """
@@ -59,22 +42,6 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY]]
             
 
-
-def this_player(board):
-    """
-    Returns player who has the next turn on a board.
-    """
-    #Return the number of non zero elements
-    elements=empty(board)
-
-    #if the board is empty, X gets the first move. Or if there is even number on board.
-    if (elements %2)==0:
-        turn="X"
-    else:
-        turn="O"
-
-    return turn
-
 def player(board):
     """
     Returns player who has the next turn on a board.
@@ -84,22 +51,24 @@ def player(board):
 
     #if the board is empty, X gets the first move. Or if there is even number on board.
     if (elements %2)==0:
-        turn="O"
-    else:
         turn="X"
+    else:
+        turn="O"
 
     return turn
 
-def actions():
+def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    act = {(0,0),(0,1),(0,2),
-            (1,0),(1,1),(1,2),
-            (2,0),(2,1),(2,2)}
+    act = set()
 
-    #if ((board[i][j] == "X") or (board[i][j] == "O")):
-    #    return act
+    for i in range(0, len(board)):
+        for j in range(0, len(board[0])):
+            if board[i][j] == EMPTY:
+                act.add((i, j))
+
+    print("act is this: ")
     print(act)
     return act
 
@@ -110,22 +79,24 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     If the grid is not already occupied, draw x or o at action.
     """
+    #make a deep copy of the board
+    state=copy.deepcopy(board)
     i=action[0]
     j=action[1]
 
-    turn=this_player(board)
+    turn=player(state)
 
-    if ((board[i][j] != "X") and (board[i][j] != "O")):
+    if ((state[i][j] != "X") and (state[i][j] != "O")):
         print("=turn returns this: ")
         print(turn)
         if (turn=="X"):
-            draw_x(board,i,j)
+            state[i][j]=turn
+            turn=O
         elif (turn=="O"):
-            draw_o(board,i,j)
+            state[i][j]=turn
+            turn=X
             
-    turn=player(board)
-
-    return board
+    return state
 
 def winner(board):
     """
@@ -140,7 +111,7 @@ def winner(board):
         [board[0][1], board[1][1], board[2][1]],
         [board[0][2], board[1][2], board[2][2]],
         [board[0][0], board[1][1], board[2][2]],
-        [board[2][0], board[1][1], board[0][2]],
+        [board[2][0], board[1][1], board[0][2]]
     ]
 
     if [turn,turn,turn] in victory:
@@ -174,4 +145,62 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    return (2,2)
+
+    if terminal(board):
+        return None
+    
+    
+    if player(board)==X:
+        max=max_val(board)
+        print("max value is")
+        print(max)
+    elif player(board)==O:
+        min=min_val(board)
+        print("min value is")
+        print(min)
+    
+    #(3,3) is the default to get stored actions
+    best=get_best((3,3))
+    print(best)
+
+    return best
+
+def max_val(state):
+    v=float('-inf')
+    if terminal(state):
+        return utility(state)
+    for action in actions(state):
+        min=min_val(result(state,action))
+        print("min is this")
+        print(min)
+        if min==None:
+            return v
+        v=max(v,min)
+        print("v is this:")
+        print(v)
+        if v==1:
+            get_best(action)
+            print("returning v")
+            return v
+
+
+def min_val(state):
+    v=float('inf')
+    if terminal(state):
+        return utility(state)
+    for action in actions(state):
+        max=max_val(result(state,action))
+        if max==None:
+            return v
+        v=min(v,max)        
+        print("min v is this:")
+        print(v)
+        if max==-1:
+            get_best(action)
+            return v
+
+def get_best(action):
+    if action !=(3,3):
+        global best_move
+        best_move=action 
+    return best_move
